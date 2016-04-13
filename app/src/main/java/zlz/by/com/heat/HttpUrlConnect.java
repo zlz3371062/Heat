8e80de0bccf7f2da2c07567e21993fc0108e8832
zlz <438580470@qq.com>
2016-04-13 11:37:49 +0800

    Initial commit

---
+++ app/src/main/java/zlz/by/com/heat/HttpUrlConnect.java
@@ -0,0 +1,101 @@
+package zlz.by.com.heat;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by mac on 16/4/13.
+ */
+public class HttpUrlConnect {
+    private  String  urlstr;
+    private  String  content;
+    private  String  resultjson;
+    private  String  key;
+
+   public  String  httpgetjson(String urlstr,String content, final String  method,String key)
+   {
+       this.urlstr  = urlstr;
+       this.content = content;
+       this.key = key;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(method == "GET"){
+                    getjson();
+
+                }else {
+                    postjson();
+
+                }
+
+
+
+            }
+        }).start();
+
+        return  resultjson;
+   }
+   private void getjson(){
+       try {
+           String str = urlstr + "key=" + key + "&" + content;
+           Log.e("zlz",key);
+           Log.e("zlz",str);
+           URL url = new URL(str);
+           HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+           BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+           StringBuilder sb = new StringBuilder();
+           String line = "";
+           while ((line = br.readLine()) != null)
+           {
+             sb.append(line);
+
+           }
+           conn.disconnect();
+           resultjson = sb.toString();
+           Log.e("zlz",resultjson  + "re");
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+
+   }
+    private  void postjson(){
+        try {
+            Log.e("zlz","post");
+            URL url = new URL(urlstr);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            bw.write(content);
+            bw.flush();
+            bw.close();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+            while ((line = br.readLine()) != null)
+            {
+                sb.append(line);
+
+            }
+            conn.disconnect();
+            resultjson = sb.toString();
+            Log.e("zlz",resultjson  + "re");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/zlz/by/com/heat/MainActivity.java
@@ -0,0 +1,35 @@
+package zlz.by.com.heat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+//    http://op.juhe.cn/onebox/hospital/queryhospital=%E5%AE%81%E6%B3%A2%E7%AC%AC%E4%B8%80%E5%8C%BB%E9%99%A2&dtype=&key=80e6cb4b13532b62ba8f00c3505a6988
+    private String surl = "http://op.juhe.cn/onebox/hospital/query.php?";
+    private String key = "80e6cb4b13532b62ba8f00c3505a6988";
+    private  String content = "hospital=宁波第一医院";
+    private String zlz = "http://zlzxm.com/he/helogin.php";
+    private  String name = "username=zlz&&password=11";
+    //207802 医院名不正确
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        HttpUrlConnect connect = new HttpUrlConnect();
+        String str = connect.httpgetjson(zlz,name,"POST",key);
+        Log.e("zlz",str + "l");
+
+
+    }
+
+
+
+}
